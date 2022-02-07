5b89bf3cbe81c5e4f1afe956d365f319e438a9f0
Stefan Laschan <st.laschan@gmail.com>
2022-02-07 16:02:17 +0100

    Add border and margin to icons

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,54 @@ import fruitsIcon from '../util/fruit-icon.jpg';
 import itemDatabase from '../util/itemDatabase';
 import meatIcon from '../util/meat-icon.jpg';
 
+const homePageStyle = css`
+  h1 {
+    display: flex;
+    justify-content: center;
+  }
+
+  h2 {
+    display: flex;
+    justify-content: center;
+  }
+
+  p {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
 const iconStyle = css`
   display: flex;
   justify-content: center;
-  border: 3px solid black;
   padding: auto;
+`;
 
-  .fruitsIcon {
-    border: 3px solid black;
-  }
+const fruitsIconStyle = css`
+  display: flex;
+  border: 3px solid black;
+  margin-left: 24px;
+  margin-right: 24px;
+`;
+
+const dairyProductsIconStyle = css`
+  display: flex;
+  border: 3px solid black;
+  margin-left: 24px;
+  margin-right: 24px;
+`;
+
+const cerealIconStyle = css`
+  display: flex;
+  border: 3px solid black;
+  margin-left: 24px;
+  margin-right: 24px;
+`;
+const meatIconStyle = css`
+  display: flex;
+  border: 3px solid black;
+  margin-left: 24px;
+  margin-right: 24px;
 `;
 
 export default function Home() {
@@ -29,12 +68,14 @@ export default function Home() {
         <title>Home</title>
         <meta name="description" content="Welcome to my website" />
       </Head>
-      <div>
-        <h1>Home page</h1>
-        <p>Home page content</p>
-        <h2>Why Choose Us</h2>
-        <p>Content why choose us</p>
-        <h2>Our Store Products</h2>
+      <div css={homePageStyle}>
+        <div>
+          <h1>Home page</h1>
+          <p>Home page content</p>
+          <h2>Why Choose Us</h2>
+          <p>WE are distributing organic products from local farmers</p>
+          <h2>Our Store Products</h2>
+        </div>
         {/* {itemDatabase.map((item) => {
           return (
             <div key={item.id}>
@@ -42,34 +83,42 @@ export default function Home() {
                 <a>{item.name}</a>
               </Link> */}
         <div css={iconStyle}>
-          <Image
-            src={fruitsIcon}
-            className="fruitsIcon"
-            alt="fruit"
-            width="200"
-            height="200"
-          />
-          <Image
-            src={dairyProductsIcon}
-            className="dairyProducts"
-            alt="fruit"
-            width="200"
-            height="200"
-          />
-          <Image
-            src={cerealIcon}
-            className="cerealIcon "
-            alt="fruit"
-            width="200"
-            height="200"
-          />
-          <Image
-            src={meatIcon}
-            className="meatIcon "
-            alt="fruit"
-            width="200"
-            height="200"
-          />
+          <div css={fruitsIconStyle}>
+            <Image
+              src={fruitsIcon}
+              className="fruitsIcon"
+              alt="fruit"
+              width="200"
+              height="200"
+            />
+          </div>
+          <div css={dairyProductsIconStyle}>
+            <Image
+              src={dairyProductsIcon}
+              className="dairyProductsIcon"
+              alt="dairyProducts"
+              width="200"
+              height="200"
+            />
+          </div>
+          <div css={cerealIconStyle}>
+            <Image
+              src={cerealIcon}
+              className="cerealIcon "
+              alt="cereal"
+              width="200"
+              height="200"
+            />
+          </div>
+          <div css={meatIconStyle}>
+            <Image
+              src={meatIcon}
+              className="meatIcon "
+              alt="meat"
+              width="200"
+              height="200"
+            />
+          </div>
         </div>
         {/* </div> */}
         {/* );
