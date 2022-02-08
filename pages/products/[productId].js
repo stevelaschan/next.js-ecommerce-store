1b93d32e7a04f5f595cad3da11f7a3c48529a98b
Stefan Laschan <st.laschan@gmail.com>
2022-02-08 17:25:27 +0100

    Add css to single product pages

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -1,8 +1,26 @@
+import { css } from '@emotion/react';
 import Head from 'next/head';
 import Image from 'next/image';
 import BuyButton from '../../components/BuyButton';
 import Layout from '../../components/Layout';
 import { getProductById } from '../../util/database';
+
+const productHeadingStyle = css`
+  display: flex;
+  justify-content: center;
+`;
+const productPictureStyle = css`
+  display: flex;
+  justify-content: center;
+  border: 2px solid black;
+`;
+
+const productDescriptionStyle = css`
+  div {
+    display: flex;
+    justify-content: center;
+  }
+`;
 
 export default function SingleProduct(props) {
   return (
@@ -11,17 +29,21 @@ export default function SingleProduct(props) {
         <title>{props.product.name}</title>
         <meta description={props.product.name} />
       </Head>
-      <h1>{props.product.name}</h1>
+
+      <h1 css={productHeadingStyle}>{props.product.name.toUpperCase()}</h1>
       <Image
+        css={productPictureStyle}
         src={`/store-products/${props.product.name}.jpg`}
         width="600"
         height="400"
       />
-      <div>id: {props.product.id}</div>
-      <div>name: {props.product.name}</div>
-      <div>product type: {props.product.type}</div>
-      <div>price: {props.product.price}</div>
-      <BuyButton />
+      <div css={productDescriptionStyle}>
+        <div>id: {props.product.id}</div>
+        <div>name: {props.product.name}</div>
+        <div>product type: {props.product.type}</div>
+        <div>price: {props.product.price}</div>
+        <BuyButton />
+      </div>
     </Layout>
   );
 }
