dc93ba437029d9363d798c8cb57c36110331f56a
Stefan Laschan <st.laschan@gmail.com>
2022-02-22 15:56:53 +0100

    Add code for deployment on heroku

--- util/database.js
+++ util/database.js
@@ -1,18 +1,29 @@
 import camelcaseKeys from 'camelcase-keys';
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
+import setPostgresDefaultsOnHeroku from './setPostgresDefaultsOnHeroku.js';
+
+setPostgresDefaultsOnHeroku();
 
 config();
 
 // Connect only once to the database
 // https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
 function connectOneTimeToDatabase() {
-  // When in development, connect only once to the database
-  if (!globalThis.postgresSqlClient) {
-    globalThis.postgresSqlClient = postgres();
-  }
-  const sql = globalThis.postgresSqlClient;
+  let sql;
 
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    sql = postgres();
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
+  }
   return sql;
 }
 
